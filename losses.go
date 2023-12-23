c4e2978e676d84bc97cd2fba764069dc278bfd1c
Josh Pattman <Josh.pattman@gmail.com>
2023-12-23 22:51:46 +0000

    Upgraded (untested) other losses

--- losses.go
+++ losses.go
@@ -13,6 +13,8 @@ import (
 // - an error
 type LossFunc func() (lossOut *G.Node, lossInps map[string]*G.Node, err error)
 
+// MSE creates the nodes to calculate mean squared error loss between a predicted and target node.
+// It should be used when using Model.Build().
 func MSELoss(targetName string, output *G.Node) LossFunc {
 	return func() (*G.Node, map[string]*G.Node, error) {
 		target := G.NewMatrix(output.Graph(), G.Float64, G.WithShape(output.Shape()...))
@@ -32,89 +34,74 @@ func MSELoss(targetName string, output *G.Node) LossFunc {
 	}
 }
 
-// MSE creates the nodes to calculate mean squared error loss between a predicted and target node.
-// It should be used when using Model.Build().
-func MSE(output, target *G.Node) (*G.Node, error) {
-	x, err := G.Sub(output, target)
-	if err != nil {
-		return nil, err
-	}
-	x, err = G.Square(x)
-	if err != nil {
-		return nil, err
-	}
-	x, err = G.Mean(x)
-	if err != nil {
-		return nil, err
-	}
-	return x, nil
-}
-
 // BCE creates the nodes to calculate binary crossentropy loss between a predicted and target node.
 // It should be used when using Model.Build().
-func BCE(output, target *G.Node) (*G.Node, error) {
-	x1, err := G.Log(output)
-	if err != nil {
-		return nil, err
+func BCELoss(targetName string, output *G.Node) LossFunc {
+	return func() (*G.Node, map[string]*G.Node, error) {
+		target := G.NewMatrix(output.Graph(), G.Float64, G.WithShape(output.Shape()...))
+		x1, err := G.Log(output)
+		if err != nil {
+			return nil, nil, err
+		}
+		x2, err := G.Sub(G.NewConstant(1.0), output)
+		if err != nil {
+			return nil, nil, err
+		}
+		x2, err = G.Log(x2)
+		if err != nil {
+			return nil, nil, err
+		}
+		x1, err = G.HadamardProd(target, x1)
+		if err != nil {
+			return nil, nil, err
+		}
+		x3, err := G.Sub(G.NewConstant(1.0), target)
+		if err != nil {
+			return nil, nil, err
+		}
+		x2, err = G.HadamardProd(x3, x2)
+		if err != nil {
+			return nil, nil, err
+		}
+		x, err := G.Add(x1, x2)
+		if err != nil {
+			return nil, nil, err
+		}
+		x, err = G.Mean(x)
+		if err != nil {
+			return nil, nil, err
+		}
+		x, err = G.Neg(x)
+		if err != nil {
+			return nil, nil, err
+		}
+		return x, map[string]*G.Node{targetName: target}, nil
 	}
-	x2, err := G.Sub(G.NewConstant(1.0), output)
-	if err != nil {
-		return nil, err
-	}
-	x2, err = G.Log(x2)
-	if err != nil {
-		return nil, err
-	}
-	x1, err = G.HadamardProd(target, x1)
-	if err != nil {
-		return nil, err
-	}
-	x3, err := G.Sub(G.NewConstant(1.0), target)
-	if err != nil {
-		return nil, err
-	}
-	x2, err = G.HadamardProd(x3, x2)
-	if err != nil {
-		return nil, err
-	}
-	x, err := G.Add(x1, x2)
-	if err != nil {
-		return nil, err
-	}
-	x, err = G.Mean(x)
-	if err != nil {
-		return nil, err
-	}
-	x, err = G.Neg(x)
-	if err != nil {
-		return nil, err
-	}
-	return x, nil
 }
 
-// CCE creates the nodes to calculate categorical crossentropy loss between a predicted and target node.
-// It should be used when using Model.Build().
-// IMPORTANT: I think this is currently broken. Either this or softmax.
-func CCE(output, target *G.Node) (*G.Node, error) {
-	x, err := G.Log(output)
-	if err != nil {
-		return nil, err
+func CCELoss(targetName string, output *G.Node) LossFunc {
+	return func() (*G.Node, map[string]*G.Node, error) {
+		target := G.NewMatrix(output.Graph(), G.Float64, G.WithShape(output.Shape()...))
+		x, err := G.Log(output)
+		if err != nil {
+			return nil, nil, err
+		}
+		x, err = G.HadamardProd(target, x)
+		if err != nil {
+			return nil, nil, err
+		}
+		x, err = G.Sum(x, 1)
+		if err != nil {
+			return nil, nil, err
+		}
+		x, err = G.Mean(x)
+		if err != nil {
+			return nil, nil, err
+		}
+		x, err = G.Neg(x)
+		if err != nil {
+			return nil, nil, err
+		}
+		return x, map[string]*G.Node{targetName: target}, nil
 	}
-	x, err = G.HadamardProd(target, x)
-	if err != nil {
-		return nil, err
-	}
-	x, err = G.Sum(x, 1)
-	if err != nil {
-		return nil, err
-	}
-	x, err = G.Mean(x)
-	if err != nil {
-		return nil, err
-	}
-	x, err = G.Neg(x)
-	if err != nil {
-		return nil, err
-	}
-	return x, nil
 }
