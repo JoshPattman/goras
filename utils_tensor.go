f296c92b67a6b22fac5600405e4fcb88eb313d86
Josh Pattman <Josh.pattman@gmail.com>
2023-12-24 23:07:24 +0000

    Added some tensor utils

---
+++ utils_tensor.go
@@ -0,0 +1,67 @@
+// The stuff in this file is just some stuff to make working with tensors easier.
+// There is a pretty good chance that this stuff is already in gorgonia, but I couldn't find it after all 2 seconds of looking I did.
+package goras
+
+import (
+	"fmt"
+
+	"gorgonia.org/tensor"
+)
+
+// Conv2DSliceToTensor converts a 2D slice to a tensor. The slice is indexed[row][column].
+func Conv2DSliceToTensor[T any](data [][]T) (tensor.Tensor, error) {
+	if len(data) == 0 || len(data[0]) == 0 {
+		return nil, fmt.Errorf("data slice must have at least one row and one column")
+	}
+	var eg T
+	typ, err := GetTensorDataType(eg)
+	if err != nil {
+		return nil, err
+	}
+	t := tensor.New(tensor.WithShape(len(data), len(data[0])), tensor.Of(typ))
+	for i, row := range data {
+		if len(row) != len(data[0]) {
+			return nil, fmt.Errorf("data slice must have the same number of columns in each row")
+		}
+		for j, v := range row {
+			if err := t.SetAt(v, i, j); err != nil {
+				return nil, err
+			}
+		}
+	}
+	return t, nil
+}
+
+// Conv1DSliceToTensor converts a 1D slice to a tensor.
+func Conv1DSliceToTensor[T any](data []T) (tensor.Tensor, error) {
+	if len(data) == 0 {
+		return nil, fmt.Errorf("data slice must have at least one element")
+	}
+	var eg T
+	typ, err := GetTensorDataType(eg)
+	if err != nil {
+		return nil, err
+	}
+	t := tensor.New(tensor.WithShape(len(data)), tensor.Of(typ))
+	for i, v := range data {
+		if err := t.SetAt(v, i); err != nil {
+			return nil, err
+		}
+	}
+	return t, nil
+}
+
+func GetTensorDataType(t interface{}) (tensor.Dtype, error) {
+	switch t.(type) {
+	case int:
+		return tensor.Int, nil
+	case float64:
+		return tensor.Float64, nil
+	case float32:
+		return tensor.Float32, nil
+	case bool:
+		return tensor.Bool, nil
+	default:
+		return tensor.Dtype{}, fmt.Errorf("unsupported type %T", t)
+	}
+}
