0cf5ebe86aae84845e023907b8ccbe325c242d2e
Josh Pattman <josh.pattman@gmail.com>
2023-12-27 22:13:08 +0000

    Added more tests

--- package_test.go
+++ package_test.go
@@ -7,7 +7,7 @@ import (
 	T "gorgonia.org/tensor"
 )
 
-func makeXORModel() *Model {
+func makeXORModel() (*Model, error) {
 	batchSize := 4
 	inputNodes, hiddenNodes, outputNodes := 2, 5, 1
 
@@ -16,14 +16,28 @@ func makeXORModel() *Model {
 	n := NewNamer("model")
 
 	inputs := Input(model, n(), batchSize, inputNodes).Node()
-	outputs := Dense(model, n(), hiddenNodes).MustAttach(inputs)
-	outputs = Activation(model, n(), "sigmoid").MustAttach(outputs)
-	outputs = Dense(model, n(), outputNodes).MustAttach(outputs)
-	outputs = Sigmoid(model, n()).MustAttach(outputs)
+	outputs, err := Dense(model, n(), hiddenNodes).Attach(inputs)
+	if err != nil {
+		return nil, err
+	}
+	outputs, err = Activation(model, n(), "sigmoid").Attach(outputs)
+	if err != nil {
+		return nil, err
+	}
+	outputs, err = Dense(model, n(), outputNodes).Attach(outputs)
+	if err != nil {
+		return nil, err
+	}
+	outputs, err = Sigmoid(model, n()).Attach(outputs)
+	if err != nil {
+		return nil, err
+	}
 
-	model.MustBuild(WithInput("x", inputs), WithOutput("yp", outputs), WithLoss(MSELoss("yt", outputs)))
-
-	return model
+	err = model.Build(WithInput("x", inputs), WithOutput("yp", outputs), WithLoss(MSELoss("yt", outputs)))
+	if err != nil {
+		return nil, err
+	}
+	return model, nil
 }
 
 func loadXORXY() (*T.Dense, *T.Dense) {
@@ -37,11 +51,16 @@ func loadXORXY() (*T.Dense, *T.Dense) {
 	)
 	return x, y
 }
+
+// TEST: Creates, trains, and tests a model that learns the XOR function. If the model incorectly predicts, this test will fail (very unlikely).
 func TestXor(t *testing.T) {
-	model := makeXORModel()
+	model, err := makeXORModel()
+	if err != nil {
+		t.Fatal(err)
+	}
 	solver := G.NewAdamSolver(G.WithLearnRate(0.01))
 	x, y := loadXORXY()
-	err := model.Fit(NamedTs{"x": x}, NamedTs{"yt": y}, solver, WithEpochs(1000), WithVerbose(false))
+	err = model.Fit(NamedTs{"x": x}, NamedTs{"yt": y}, solver, WithEpochs(1000), WithVerbose(false))
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -60,5 +79,97 @@ func TestXor(t *testing.T) {
 	d, _ := yp.At(3, 0)
 	if a.(float64) > 0.1 || b.(float64) < 0.9 || c.(float64) < 0.9 || d.(float64) > 0.1 {
 		t.Fatal("wrong output: ", yp)
+	}
+}
+
+// Dosent build the model
+func makeUnfinishedXORModel() (*Model, *G.Node, *G.Node, error) {
+	batchSize := 4
+	inputNodes, hiddenNodes, outputNodes := 2, 5, 1
+
+	model := NewModel(T.Float64)
+
+	n := NewNamer("model")
+
+	inputs := Input(model, n(), batchSize, inputNodes).Node()
+	outputs, err := Dense(model, n(), hiddenNodes).Attach(inputs)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	outputs, err = Activation(model, n(), "sigmoid").Attach(outputs)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	outputs, err = Dense(model, n(), outputNodes).Attach(outputs)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	outputs, err = Sigmoid(model, n()).Attach(outputs)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	return model, inputs, outputs, nil
+}
+func TestMSELoss(t *testing.T) {
+	model, inputs, outputs, err := makeUnfinishedXORModel()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = model.Build(WithInput("x", inputs), WithOutput("yp", outputs), WithLoss(MSELoss("yt", outputs)))
+	if err != nil {
+		t.Fatal(err)
+	}
+	solver := G.NewAdamSolver(G.WithLearnRate(0.01))
+	x, y := loadXORXY()
+	err = model.Fit(NamedTs{"x": x}, NamedTs{"yt": y}, solver, WithEpochs(1), WithVerbose(false))
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = model.Predict(NamedTs{"x": x})
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestBCELoss(t *testing.T) {
+	model, inputs, outputs, err := makeUnfinishedXORModel()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = model.Build(WithInput("x", inputs), WithOutput("yp", outputs), WithLoss(BCELoss("yt", outputs)))
+	if err != nil {
+		t.Fatal(err)
+	}
+	solver := G.NewAdamSolver(G.WithLearnRate(0.01))
+	x, y := loadXORXY()
+	err = model.Fit(NamedTs{"x": x}, NamedTs{"yt": y}, solver, WithEpochs(1), WithVerbose(false))
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = model.Predict(NamedTs{"x": x})
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestCCELoss(t *testing.T) {
+	model, inputs, outputs, err := makeUnfinishedXORModel()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = model.Build(WithInput("x", inputs), WithOutput("yp", outputs), WithLoss(CCELoss("yt", outputs)))
+	if err != nil {
+		t.Fatal(err)
+	}
+	solver := G.NewAdamSolver(G.WithLearnRate(0.01))
+	x, y := loadXORXY()
+	err = model.Fit(NamedTs{"x": x}, NamedTs{"yt": y}, solver, WithEpochs(1), WithVerbose(false))
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = model.Predict(NamedTs{"x": x})
+	if err != nil {
+		t.Fatal(err)
 	}
 }
